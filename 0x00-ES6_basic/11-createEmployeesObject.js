dd91223eab7d45b53d735391fb4d53a62a52bc2b
avonggeorge <georgeavong@yahoo.com>
2024-10-03 01:03:39 +0100

    Corrected some indentation

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-    const employee = {
-      departmentName: employees,
-    };
-    return employee;
-  }
-  
+  const employee = {
+    departmentName: employees,
+  };
+
+  return employee;
+}
