9baac8009e0c16370bfaacbd266599c4f35b3186
avonggeorge <georgeavong@yahoo.com>
2024-10-12 00:51:41 +0100

    A function named hasValuesFromArray that returns a boolean if all the elements in the array exist within the set.

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,6 @@
+// Define the function
+export default function hasValuesFromArray(set, array) {
+    // Check if all elements in the array exist in the set
+    return array.every(element => set.has(element));
+  }
+  
