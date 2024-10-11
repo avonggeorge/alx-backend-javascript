c87dbb93f81c5637883cc6a5ce112099496bea2d
avonggeorge <georgeavong@yahoo.com>
2024-10-12 00:30:40 +0100

    Modified the documentation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,16 @@
-// Define the function
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @author Bezaleel Olakunori <https://github.com/B3zaleel>
+* @returns
+*/
 export default function getListStudentIds(students) {
-  // Check if the argument is not an array
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  // Use map() to return an array of student ids
-  return students.map(student => student.id);
+ if (students instanceof Array) {
+   return students.map((student) => student.id);
+ }
+ return [];
 }
